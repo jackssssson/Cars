0b946c54e891bd6966a0c3b29fe77371b055837e
jivko <jivko_hinev90@abv.bg>
2018-09-04 20:46:26 +0300

    Parse json file from server, create car class and parse to string, create listView with cars.

--- app/src/main/java/com/cars/cars/MainActivity.java
+++ app/src/main/java/com/cars/cars/MainActivity.java
@@ -1,22 +1,32 @@
 package com.cars.cars;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
-import okhttp3.Call;
-import okhttp3.Callback;
+import cars.Car;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mText;
+    private List<Car> listOfCars;
+    private ListView listViewCars;
 
     @SuppressLint("StaticFieldLeak")
     @Override
@@ -24,31 +34,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mText = findViewById(R.id.textView);
-        String url = "http://10.86.177.253:8080/api/cars";
+        listOfCars = new ArrayList<>();
+        listViewCars = findViewById(R.id.list_View_Cars);
+
+        AsyncTaskRequest();
+
+        System.out.println();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public void AsyncTaskRequest(){
+        String url = "http://79.100.147.125:8080/api/cars";
 
         final OkHttpClient client = new OkHttpClient();
 
         final Request request = new Request.Builder()
                 .url(url)
                 .build();
-
-        /*
-        new AsyncTask<Boolean, Void, Void>(){
-            @Override
-            protected Void doInBackground(Boolean... booleans) {
-                try {
-                    Response response = client.newCall(request).execute();
-                    String body = response.body().string();
-                    mText.setText(body);
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
-
-                return null;
-            }
-        }.execute(true);
-        */
 
         new AsyncTask<Boolean, Void, Void>() {
 
@@ -57,12 +59,14 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     Response response = client.newCall(request).execute();
                     if (response.isSuccessful()) {
-                        final String myResponce = response.body().string();
+                        final String myResponce = Objects.requireNonNull(response.body()).string();
 
                         MainActivity.this.runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                mText.setText(myResponce);
+                                listOfCars = gson(myResponce);
+                                String[] carNames = carInfoNames(listOfCars);
+                                showListView(carNames);
                             }
                         });
                     }
@@ -72,6 +76,38 @@ public class MainActivity extends AppCompatActivity {
                 return null;
             }
         }.execute(true);
+    }
 
+    private void showListView(String[] carNames) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, carNames);
+
+        listViewCars.setAdapter(adapter);
+        listViewCars.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(MainActivity.this, CarImage.class));
+            }
+        });
+    }
+
+    public List<Car> gson(String s) {
+        Gson gs = new Gson();
+
+        Car[] car = gs.fromJson(s, Car[].class);
+
+        return new ArrayList<>(Arrays.asList(car));
+    }
+
+    public String[] carInfoNames(List<Car> cars){
+        String[] carNames = new String[listOfCars.size()];
+        for (int i = 0; i < cars.size(); i++){
+            Car car = cars.get(i);
+
+            String s = car.toString();
+            carNames[i] = s;
+        }
+
+        return carNames;
     }
 }
